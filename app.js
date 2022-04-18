e0b9f19571e783273bfb0372cc0bdacb4b66978c
Austin Sims <adsims2001@gmail.com>
2022-04-18 14:12:22 -0500

    dedupe

--- app.js
+++ app.js
@@ -20,46 +20,47 @@ const candidates = [
 
 const scores = new Map(candidates.map(c => [c, 0]));
 
-const x = [];
-for (let a = 0; a < candidates.length; a++) {
-  for (let b = 0; b < candidates.length; b++) {
-    x.push(Immutable.Set([ candidates[a], candidates[b] ]));
-  }
-}
-const matchups = Immutable.Set(x);
-console.log(matchups);
-
+const builder = [];
 for (let a = 0; a < candidates.length; a++) {
   for (let b = 0; b < candidates.length; b++) {
     if (a === b) continue;
-    document.body.innerHTML = `
+    builder.push(Immutable.Set([candidates[a], candidates[b]]));
+  }
+}
+const matchups = Immutable.Set(builder);
+
+for (const matchup of matchups) {
+  const array = matchup.toArray();
+  const candidateA = array[0];
+  const candidateB = array[1];
+
+  document.body.innerHTML = `
       <h1>
-        [A] ${candidates[a]}
+        [A] ${candidateA}
         <br /> --or --
-        <br /> [B] ${candidates[b]}
+        <br /> [B] ${candidateB}
       </h1>
     `;
 
-    const msg = `[A] ${candidates[a]}  --or--  [B] ${candidates[b]}`;
-    const resp = prompt(msg);
-    switch (resp) {
-      case 'a':
-        scores.set(candidates[a], scores.get(candidates[a]) + 1);
-        scores.set(candidates[b], scores.get(candidates[b]) - 1);
-        break;
-      case 'b':
-        scores.set(candidates[a], scores.get(candidates[a]) - 1);
-        scores.set(candidates[b], scores.get(candidates[b]) + 1);
-        break;
-      default:
-        alert(`expected 'a' or 'b' but got ${resp}`);
-        b = b - 1;
-    }
+  const msg = `[A] ${candidateA}  --or--  [B] ${candidateB}`;
+  const resp = prompt(msg);
+  switch (resp) {
+    case 'a':
+      scores.set(candidateA, scores.get(candidateA) + 1);
+      scores.set(candidateB, scores.get(candidateB) - 1);
+      break;
+    case 'b':
+      scores.set(candidateA, scores.get(candidateA) - 1);
+      scores.set(candidateB, scores.get(candidateB) + 1);
+      break;
+    default:
+      alert(`expected 'a' or 'b' but got ${resp}`);
+      b = b - 1;
   }
 }
 
 const results = [...scores.entries()]
-    .sort((left, right) => right[1] - left[1]);
+  .sort((left, right) => right[1] - left[1]);
 
 let html = ``;
 html += `<table>`;
@@ -77,7 +78,7 @@ for (let i = 0; i < results.length; i++) {
   const [candidate, score] = results[i];
   html += `
     <tr>
-      <td>${i+1}</td>
+      <td>${i + 1}</td>
       <td>${candidate}</td>
       <td>${score}</td>
     </tr>
